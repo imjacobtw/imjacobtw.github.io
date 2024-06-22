7032a2cbbd7f26c8947132cfae40a9ac1b9c97a8
imjacobtw <jtwilkerson@proton.me>
2024-06-22 01:20:09 -0500

    fix: Added margin to landing page paragraph.

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function navMenuOpen() {
     document.getElementById('nav-menu').style.transform = 'translateY(0%)'
     document.querySelector('body').style.overflowY = 'hidden'
-    document.querySelector('main').style.overflowY = 'hidden'
 }
 
 function navMenuClose() {
     document.getElementById('nav-menu').style.transform = 'translateY(-100%)'
     document.querySelector('body').style.overflowY = 'visible'
-    document.querySelector('main').style.overflowY = 'visible'
 }
 
